4495ad11b9049481cbf04b92f461a6da91eeae5d
Jindapxnn <jindapol.koh@gmail.com>
2025-09-15 22:57:10 +0700

    Bugfix and Configure Package and CI workflow This commit consolidates all setup changes for a robust development and production enviroment.
    - Adds and config core dependencies (@types/node, typescript).
    - Update CI workflow (separate between lint and project build).
    - Resolves package conflicts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>{children}</Providers>
-      </body>
-    </html>
+    <Providers>
+      <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </div>
+    </Providers>
   );
 }
