03ef7b59311091803d35bb7638fa0e99fe43f9ea
Jindapxnn <jindapol.koh@gmail.com>
2025-09-30 09:49:11 +0700

    Config: db_secret_template.txt (delete comment)

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,23 +5,21 @@ resource "random_id" "bucket_suffix" {
 
 # สร้าง S3 Bucket
 resource "aws_s3_bucket" "main" {
-  # ชื่อ Bucket จะเป็น "dockeeper-uploads-xxxxxxxx"
-  # สามารถใช้แบบนี้เพื่อกันชื่อซ้ำ  "${var.project_name}-bucket-${random_id.bucket_suffix.hex}"
   bucket = "${var.project_name}-bucket"
-
+  
   tags = {
-    Name        = "${var.project_name}-bucket"
-    Project     = var.project_name
+    Name    = "${var.project_name}-bucket"
+    Project = var.project_name
   }
 }
 
 # ตั้งค่าการเข้ารหัสข้อมูลฝั่ง Server (Server-Side Encryption)
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   bucket = aws_s3_bucket.main.id
-
+  
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      sse_algorithm = "AES256"
     }
   }
 }
@@ -29,6 +27,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
 # เปิดใช้งานการเก็บเวอร์ชันของไฟล์ (Versioning)
 resource "aws_s3_bucket_versioning" "main" {
   bucket = aws_s3_bucket.main.id
+  
   versioning_configuration {
     status = "Enabled"
   }
@@ -37,7 +36,7 @@ resource "aws_s3_bucket_versioning" "main" {
 # ตั้งค่าการบล็อกการเข้าถึงแบบสาธารณะ (Block Public Access)
 resource "aws_s3_bucket_public_access_block" "main" {
   bucket = aws_s3_bucket.main.id
-
+  
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
@@ -47,8 +46,7 @@ resource "aws_s3_bucket_public_access_block" "main" {
 # เพิ่ม Policy เพื่ออนุญาตให้ใครก็ได้สามารถอ่านไฟล์ได้ (GetObject)
 resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.main.id
-
-  # policy คือโค้ด JSON ที่เราใช้กำหนดสิทธิ์
+  
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -57,8 +55,13 @@ resource "aws_s3_bucket_policy" "public_read" {
         Effect    = "Allow",
         Principal = "*",
         Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.main.arn}/*" # อนุญาตทุกไฟล์ใน Bucket นี้
+        Resource  = "${aws_s3_bucket.main.arn}/*"
       }
     ]
   })
+  
+  # สำคัญมาก: รอให้ public access block ถูกปิดก่อน
+  depends_on = [
+    aws_s3_bucket_public_access_block.main
+  ]
 }
